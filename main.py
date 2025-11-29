bb12dc54904009c6217c5573451fe19d8e5620a5
jprincip21 <jprincip21@gmail.com>
2025-11-29 01:39:40 -0500

    Return temperature for any station and data available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__) # Create a Flask instance named Website
 
@@ -9,7 +10,15 @@ def home():
 
 @app.route("/api/v1/<station>/<date>") # <> Means user can enter a value
 def about(station, date):
-    temperature = 23
+    # .zfill(6) Takes the station ID and ensures it is 6 digits, the value provided will be on the right and added values will be 0
+    # ie, station = 10, .zfill(6) outputs 000010
+    filename = "data/TG_STAID" + str(station).zfill(6) + ".txt"
+    print(filename)
+
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"]) # Read Data for user requested station, skips unneeded info and parses dates
+
+    # Grabs the cell at specified date for the TG column. .squeeze() allows us to return only the  value from that cell and ignore the index
+    temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze() / 10
 
     return {"station": station, # Taken from URL
             "date": date, # Taken from URL
@@ -17,6 +26,6 @@ def about(station, date):
             }
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 
 
